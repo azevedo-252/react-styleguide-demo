0b92aa470134c543ee6955c02b6a5d9ad524402c
Bruno Azevedo <azevedo.252@gmail.com>
2015-09-25 16:43:06 +0100

    --wip--

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
+var browserify = require('browserify');
+var react = require('react');
+var path = require('path');
 var express = require('express');
 var app = express();
+var cons = require('consolidate');
+app.engine('html', cons.lodash);
+app.set('view engine', 'html');
+app.set('views', '.');
+
+var basepath = path.join(__dirname);
 
 
 /************************************************************
@@ -33,6 +42,82 @@ app.get('/style.css', function(req, res) {
   }
 });
 
+
+
+
+
+var glob = require('glob');
+
+function getAllAppComponentSandboxNames(callback) {
+  glob("src/components/**/sandbox.jsx", function (er, files) {
+    callback(files.map(function (path) {
+      return path.split('/')[2];
+    }));
+  });
+}
+
+function toPath() {
+  var args = Array.prototype.slice.call(arguments);
+
+  return args.join('/');
+}
+
+app.get('/components/:component.js', function(req, res, next) {
+  var filename = path.join(basepath, './src/components', req.params.component, req.params.component + '.jsx');
+  var sandbox = path.join(basepath, './src/components', req.params.component, 'sandbox.jsx');
+  var b = browserify();
+
+  b.require('react', {expose: 'react'});
+  b.require(filename, {expose: req.params.component});
+  b.require(sandbox, {expose: 'sandbox'});
+
+  res.setHeader('content-type', 'text/javascript');
+
+  // catch file system errors, such as test.js being unreadable
+  b.on('error', function(error) {
+    console.error('browserify error', error);
+
+    res.send('console.error(\'' + errorMessage + '\');');
+  });
+
+  b.bundle()
+    .on('error', function(error) {
+      console.log("b.bundle() error", error);
+
+      var errorMessage = [error.name, ': "', error.description, '" in ', error.filename, ' at line number ', error.lineNumber].join('');
+      // due to Chrome not displaying response data in non 200 states need to expose the error message via a console.error
+      res.send('console.error(\'' + errorMessage + '\');');
+    })
+    .pipe(res);
+});
+
+app.get('/components/:component', function(req, res) {
+  var name = req.params.component;
+  var symbol = name;
+  var uri = toPath('/components/' + symbol + '.js');
+
+  res.render('component', {
+    component_uri: uri,
+    component_name: name
+  });
+});
+
+app.get('/components', function(req, res) {
+  getAllAppComponentSandboxNames(function(componentNames) {
+    res.render('components', {
+      components: componentNames
+    });
+  });
+});
+
+
+
+
+
+
+
+
+
 // Serve index page
 app.get('*', function(req, res) {
   res.sendFile(__dirname + '/build/index.html');
@@ -49,7 +134,6 @@ app.post('/home', function(req, res) {
     title: "Home Page"
   });
 });
-
 
 /*************************************************************
  *
---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+// import CSSModules from 'react-css-modules';
+// import styles from "./Button.css";
+//
+// @CSSModules(styles)
+export default class Button extends React.Component {
+  render() {
+    return (
+      <div styleName="root">
+        My Button
+      </div>
+    )
+  }
+}
